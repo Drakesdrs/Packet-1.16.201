c29bf35bf9940830f2fde54000832b76c4f10e52
Drakesdrs <67423571+Drakesdrs@users.noreply.github.com>
2021-08-08 20:00:28 +0100

    Update Hooks.cpp

--- Memory/Hooks.cpp
+++ Memory/Hooks.cpp
@@ -679,9 +679,9 @@ __int64 Hooks::RenderText(__int64 a1, C_MinecraftUIRenderContext* renderCtx) {
 					constexpr float borderPadding = 1;
 					constexpr float margin = 5;
 
-					static std::string name = "Packet Client";
+					static std::string name = "SkidX v3";
 #ifdef _DEBUG
-					static std::string version = " ";
+					static std::string version = "";
 #elif defined _BETA
 					static std::string version = " ";
 #else
